e151bdac2b1b0daee5536c0a1221e17e63b86c48
ttkkmmaa9 <vuminhvuong981@gmail.com>
2025-05-17 08:13:27 +0700

    upload full site

---
+++ js/cookieconsent-config.js
@@ -0,0 +1,58 @@
+﻿/**
+ * All config. options available here:
+ * https://cookieconsent.orestbida.com/reference/configuration-reference.html
+ */
+CookieConsent.run({
+
+    categories: {
+        necessary: {
+            enabled: true,  // this category is enabled by default
+            readOnly: true  // this category cannot be disabled
+        },
+        analytics: {}
+    },
+
+    language: {
+        default: 'vn',
+        translations: {
+            vn: {
+                consentModal: {
+                    title: 'Sử dụng cookie?',
+                    description: 'Website có thể sử dụng cookie của bạn nhằm mục đích lưu trữ dữ liệu cá nhân hóa của bạn',
+                    acceptAllBtn: 'Đồng ý',
+                    acceptNecessaryBtn: 'Từ chối',
+                   
+                },
+                preferencesModal: {
+                    title: 'Manage cookie preferences',
+                    acceptAllBtn: 'Accept all',
+                    acceptNecessaryBtn: 'Reject all',
+                    savePreferencesBtn: 'Accept current selection',
+                    closeIconLabel: 'Close modal',
+                    sections: [
+                        {
+                            title: 'Somebody said ... cookies?',
+                            description: 'I want one!'
+                        },
+                        {
+                            title: 'Strictly Necessary cookies',
+                            description: 'These cookies are essential for the proper functioning of the website and cannot be disabled.',
+
+                            //this field will generate a toggle linked to the 'necessary' category
+                            linkedCategory: 'necessary'
+                        },
+                        {
+                            title: 'Performance and Analytics',
+                            description: 'These cookies collect information about how you use our website. All of the data is anonymized and cannot be used to identify you.',
+                            linkedCategory: 'analytics'
+                        },
+                        {
+                            title: 'More information',
+                            description: 'For any queries in relation to my policy on cookies and your choices, please <a href="#contact-page">contact us</a>'
+                        }
+                    ]
+                }
+            }
+        }
+    }
+});
---
+++ themes/dvp/javascript/slider2.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+   
+     jQuery('.tp-banner').revolution(
+             {
+                 delay: 5000,
+                 startwidth: 1170,
+                 startheight: 530,
+                 hideThumbs: 10,
+                 fullWidth: "on",
+                 forceFullWidth: "on",
+                 hideCaptionAtLimit: 650
+             });
+});
